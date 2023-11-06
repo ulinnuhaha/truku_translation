8e29c35428fb6f8d67f7d2c99c93742d281bb9e6
Ulin Nuha <ulinpoenya@gmail.com>
2023-11-06 11:56:05 +0800

    Update preprocessing.py

--- dataset/preprocessing.py
+++ dataset/preprocessing.py
@@ -1,11 +1,6 @@
 import os
-import torch
-import random
 import pandas as pd
 import numpy as np
-import torch.nn.functional as F
-from time import perf_counter
-from datasets import load_dataset
 from sklearn.model_selection import train_test_split
 
 #Load the raw dataset
