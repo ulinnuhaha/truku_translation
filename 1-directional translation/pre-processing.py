4bbe09ceebe0b2c2c58885f1a96650dda09d1e8e
Ulin Nuha <ulinpoenya@gmail.com>
2023-11-06 20:42:25 +0800

    Update pre-processing.py

--- 1-directional translation/pre-processing.py
+++ 1-directional translation/pre-processing.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 
 #Load the raw dataset
 def get_data(file,direct):
-    df=pd.read_excel('./'+file+'.xlsx')
+    df=pd.read_excel('./datasets/'+file+'.xlsx')
     df =df.replace(to_replace=r'_x000D_', value='', regex=True) #delete _x000D_ found in the element/
     df=df.rename(columns={"華語": "chinese", "太魯閣族語": "truku"}) #rename columns
     if direct== 'chi2tr': #create dataset where chinese as source language 
@@ -19,13 +19,7 @@ def create_bitext_data(direct):
     t1=get_data('其他來源',direct)
     t2=get_data('字根句型辭典',direct)
     t3=get_data('族語E樂園',direct)
-    t4=get_data('聖經新舊約',direct) #bible
-    
-    if direct == 'chi2tr':
-        prefix = '將華語成太魯閣族語: '
-    else:
-        prefix = '將太魯閣族語成華語: '
-        
+    t4=get_data('聖經新舊約',direct) #bible       
     data_all = train=pd.concat([t1,t2,t3,t4]) #group all dataset
     data_all['source_lang'] = prefix + data_all['source_lang'].astype(str) # Put the prefix in each sentence of source language
     data_all=data_all.drop_duplicates(subset=['target_lang']) #drop duplicate row
@@ -45,16 +39,14 @@ print('Starting to create: Truku to Chinese dataset')
 direct='tr2chi'
 tc_train, tc_test, tc_val = create_bitext_data(direct)
 print('Successfully creating: Truku to Chinese dataset')
-## Group all data
-train = pd.concat([tc_train, ct_train])
-train = train.sample(frac=1) #shuffle the data
-val = pd.concat([tc_val, ct_val])
-val = val.sample(frac=1) #shuffle the data
 
-#Save the data to tsv
-train.to_csv('train.tsv', sep="\t", index=False,header=False)
-val.to_csv('val.tsv', sep="\t", index=False,header=False)
-#We set the testing data in two files since the evaluation metrics for Chinese to Truku & Truku to Chinese is distinct
-tc_test.to_csv('test_tru2chi.tsv', sep="\t", index=False,header=False) #testing data for Chinese to Truku translation
-ct_test.to_csv('test_chi2tru.tsv', sep="\t", index=False,header=False) #testing data for Truku to Chinese translation
+#Save the data for Truku to Chinese translation 
+ct_train.to_csv('train_chi2tru.tsv', sep="\t", index=False,header=False)
+ct_val.to_csv('val_chi2tru.tsv', sep="\t", index=False,header=False)
+ct_test.to_csv('test_chi2tru.tsv', sep="\t", index=False,header=False) 
+
+#Save the data for Chinese to Truku translation 
+tc_train.to_csv('train_tru2chi.tsv', sep="\t", index=False,header=False)
+tc_val.to_csv('val_tru2chi.tsv', sep="\t", index=False,header=False)
+tc_test.to_csv('test_tru2chi.tsv', sep="\t", index=False,header=False) 
 print('Successfully saving all dataset')
