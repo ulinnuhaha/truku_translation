1a4f5d2641a22533e810a488f1fd72b1c5ae6688
Ulin Nuha <ulinpoenya@gmail.com>
2023-11-06 14:02:43 +0800

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -4,13 +4,8 @@
 import os
 import torch
 import random
-import pandas as pd
 import evaluate
 import numpy as np
-import matplotlib.pyplot as plt
-import torch.nn as nn
-import torch.nn.functional as F
-from typing import Optional
 from dataclasses import dataclass
 from time import perf_counter
 from datasets import load_dataset
