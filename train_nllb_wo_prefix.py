8ede59ebb9e5c9104dbe1ccf21c35f52b2e6e8fb
Ulin Nuha <ulinpoenya@gmail.com>
2023-12-28 15:50:24 +0800

    Create train_nllb_wo_prefix.py

---
+++ train_nllb_wo_prefix.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import torch
+import random
+import evaluate
+import numpy as np
+from dataclasses import dataclass
+from time import perf_counter
+from datasets import load_dataset, DatasetDict, concatenate_datasets
+from torch.utils.data import Dataset, DataLoader
+import argparse
+from transformers import (
+    NllbTokenizer,
+    AutoModelForSeq2SeqLM,
+    DataCollatorForSeq2Seq,
+    Seq2SeqTrainingArguments,
+    Seq2SeqTrainer,
+    EarlyStoppingCallback
+)
+
+def DataTrainingArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_checkpoint', 
+        type=str, required=True, help='Load a LLM as model checkpoint for translation')
+    parser.add_argument('--cache_dir', 
+        type=str, required=True, help='Directory for saving the pre-trained translation model')
+    parser.add_argument('--data_dir', 
+        type=str, required=True, help='Directory of the dataset files')
+    args = parser.parse_args()
+    return args
+
+#create the configuration class
+@dataclass
+class Config:
+    lang: str = "tr_ch"
+    batch_size: int = 16
+    num_workers: int = 4
+    seed: int = 42
+    max_source_length: int = 128
+    max_target_length: int = 128
+
+    lr: float = 0.0005
+    weight_decay: float = 0.01
+    epochs: int = 20
+    device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    #model_checkpoint: str = "facebook/nllb-200-distilled-600M"
+
+    def __post_init__(self):
+        random.seed(self.seed)
+        np.random.seed(self.seed)
+        torch.manual_seed(self.seed)
+        torch.cuda.manual_seed_all(self.seed)
+
+def main():
+    data_train_args=DataTrainingArguments()
+    config = Config()
+    #Load the dataset from tsv files
+    onfig = Config()
+
+    def get_data(langs):
+        data_file = {}
+        for split in ["train", "val"]:
+            output_path = os.path.join(config.data_dir, langs+f"_{split}.tsv")
+            data_file[split] = [output_path]
+
+        dataset_dict = load_dataset(
+            "csv",
+            delimiter="\t",
+            #column_names=["source_lang", "target_lang"],
+            data_files=data_file)
+        return dataset_dict
+    dataset_dict_tc=get_data('tc')
+    dataset_dict_ct=get_data('ct')
+    print(dataset_dict_tc)
+  
+    #Load the evaluation metrics
+    rouge_score = evaluate.load("rouge")
+    bleu_score = evaluate.load("bleu")
+    sacrebleu_score = evaluate.load("sacrebleu")
+    chrf_score = evaluate.load("chrf")
+    
+    # Load the tokenizer and model from pre-trained LLMs
+    tokenizer = NllbTokenizer.from_pretrained(data_train_args.model_checkpoint)
+    
+    def fix_tokenizer(tokenizer, new_lang='tru_Latn'):
+        """ Add a new language token to the tokenizer vocabulary (this should be done each time after its initialization) """
+        old_len = len(tokenizer) - int(new_lang in tokenizer.added_tokens_encoder)
+        tokenizer.lang_code_to_id[new_lang] = old_len-1
+        tokenizer.id_to_lang_code[old_len-1] = new_lang
+        # always move "mask" to the last position
+        tokenizer.fairseq_tokens_to_ids["<mask>"] = len(tokenizer.sp_model) + len(tokenizer.lang_code_to_id) + tokenizer.fairseq_offset
+
+        tokenizer.fairseq_tokens_to_ids.update(tokenizer.lang_code_to_id)
+        tokenizer.fairseq_ids_to_tokens = {v: k for k, v in tokenizer.fairseq_tokens_to_ids.items()}
+        if new_lang not in tokenizer._additional_special_tokens:
+            tokenizer._additional_special_tokens.append(new_lang)
+        # clear the added token encoder; otherwise a new token may end up there by mistake
+        tokenizer.added_tokens_encoder = {}
+        tokenizer.added_tokens_decoder = {}
+
+    if len(tokenizer) != tokenizer.vocab_size: #Check whether the values between len(tokenizer) and tokenizer.vocab_size are same after we added Truku language tag
+        # This is only performed when we already expanded the tokenizer of the NLLB model
+        print("fix the tokenizer configuration")
+        fix_tokenizer(tokenizer)
+
+    model_name = data_train_args.model_checkpoint.split("/")[-1] #the name of pre-trained model
+    
+    fine_tuned_model_checkpoint = os.path.join(
+        data_train_args.cache_dir,
+        f"{model_name}_{config.lang}"
+    )
+    
+    if os.path.isdir(fine_tuned_model_checkpoint): #load the pre-trained translation model if available
+        do_train = False
+        model = AutoModelForSeq2SeqLM.from_pretrained(fine_tuned_model_checkpoint, cache_dir=data_train_args.cache_dir)
+    else: #load the checkpoint model from LLMS as initial checkpoint for translation model
+        do_train = True
+        model = AutoModelForSeq2SeqLM.from_pretrained(data_train_args.model_checkpoint, cache_dir=data_train_args.cache_dir)
+    
+    print("number of parameters:", model.num_parameters())
+    def batch_tokenize_fn(examples):
+        """
+        Generate the input_ids and labels field for huggingface dataset/dataset dict.
+    
+        Truncation is enabled where we cap the sentence to the max length. Padding will be done later
+        in a data collator, so we pad examples to the longest length within a mini-batch and not
+        the whole dataset.
+        """
+        #set the language source and targer
+        src = (list(examples.keys()))[0]
+        tgt = (list(examples.keys()))[1]
+        sources = examples[src]
+        targets = examples[tgt]
+            
+        src = 'tru_Latn' if src == 'truku' else 'zho_Hant'
+        tgt = 'tru_Latn' if tgt == 'truku' else 'zho_Hant'
+        
+        tokenizer.src_lang = src
+        model_inputs = tokenizer(sources, max_length=config.max_source_length, truncation=True)
+    
+        # setup the tokenizer for targets,
+        # huggingface expects the target tokenized ids to be stored in the labels field
+      
+        tokenizer.src_lang = tgt
+        labels = tokenizer(targets, max_length=config.max_target_length, truncation=True)
+    
+        model_inputs["labels"] = labels["input_ids"]
+        return model_inputs
+        
+    #tokenize truku and chinese dataset
+    tc_dataset_dict_tokenized = dataset_dict_tc.map(
+        batch_tokenize_fn,
+        batched=True,
+        remove_columns=dataset_dict_tc["train"].column_names)
+    ct_dataset_dict_tokenized = dataset_dict_ct.map(
+        batch_tokenize_fn,
+        batched=True,
+        remove_columns=dataset_dict_ct["train"].column_names)
+    #concatenate all dataset
+    dtc=tc_dataset_dict_tokenized
+    dct=ct_dataset_dict_tokenized
+    all_data=DatasetDict({"train": concatenate_datasets([dtc["train"], dct["train"]]),"val": concatenate_datasets([dtc["val"], dct["val"]])})
+    all_data_tokenized=all_data.shuffle(seed=42)
+    print(all_data_tokenized["train"][278])
+    
+    output_dir = os.path.join(data_train_args.cache_dir, f"{model_name}_{config.lang}") # where the pre-trained translation model is saved
+    
+    #The training arguments for the training session
+    args = Seq2SeqTrainingArguments(
+        output_dir=output_dir,
+        evaluation_strategy="steps",
+        learning_rate=config.lr,
+        per_device_train_batch_size=config.batch_size,
+        per_device_eval_batch_size=config.batch_size,
+        weight_decay=config.weight_decay,
+        save_total_limit=2,
+        num_train_epochs=config.epochs,
+        predict_with_generate=True,
+        load_best_model_at_end=True,
+        greater_is_better=False, #lower score better result of the main metric
+        metric_for_best_model="eval_loss", #set metrics as the main parameter
+        gradient_accumulation_steps=8,
+        do_train=do_train,
+        # https://discuss.huggingface.co/t/mixed-precision-for-bfloat16-pretrained-models/5315
+        fp16=False
+    )
+    # evalution metrics computation
+    def compute_metrics(eval_pred):
+        """
+        Compute rouge and bleu metrics for seq2seq model generated prediction.
+        
+        tip: we can run trainer.predict on our eval/test dataset to see what a sample
+        eval_pred object would look like when implementing custom compute metrics function
+        """
+        predictions, labels = eval_pred
+        # Decode generated summaries, which is in ids into text
+        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        # Replace -100 in the labels as we can't decode them
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        # Decode labels, a.k.a. reference summaries into text
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+        result = rouge_score.compute(
+            predictions=decoded_preds,
+            references=decoded_labels,
+            rouge_types=["rouge1", "rouge2", "rougeL"] #A ROUGE score close to zero indicates poor similarity between candidate and references. A ROUGE score close to one indicates strong similarity between candidate and references
+        )
+        score = sacrebleu_score.compute(
+            predictions=decoded_preds,
+            references=decoded_labels
+        )
+        chrf=chrf_score.compute(predictions=decoded_preds, references=decoded_labels)
+        result["sacrebleu"] = score["score"] #The higher the value, the better the translations
+        result["chrf"] = chrf["score"]
+        return {k: round(v, 4) for k, v in result.items()}
+        
+    # Data collator used for seq2seq model
+    data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+    trainer = Seq2SeqTrainer(
+        model,
+        args,
+        train_dataset=all_data_tokenized["train"],
+        eval_dataset=all_data_tokenized["val"],
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+        callbacks=[EarlyStoppingCallback(2, 0.0)] #early_stopping_patience =2, early_stopping_threshold =0
+    )
+    
+    # perform the training process
+    if trainer.args.do_train:
+        t1_start = perf_counter()
+        train_output = trainer.train()
+        t1_stop = perf_counter()
+        print("Training elapsed time:", t1_stop - t1_start)
+    
+        # saving the pre-trained model
+        trainer.save_model(fine_tuned_model_checkpoint)
+    
+    trainer.evaluate()
+if __name__ == "__main__":
+    main()
