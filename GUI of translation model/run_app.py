3dd09d79f261d3c56f2d7e2594e0402ce6d21c2a
Ulin Nuha <ulinpoenya@gmail.com>
2023-12-29 17:35:02 +0800

    Update run_app.py

--- GUI of translation model/run_app.py
+++ GUI of translation model/run_app.py
@@ -4,12 +4,12 @@ import gradio as gr
 from translation import Translator, DIRECTION
 DIRECTION_LIST = list(DIRECTION.keys())
 
-def translate_wrapper(text, dir, by_sentence=True, preprocess=True, random=False, num_beams=4):
+def translate_wrapper(dir, text_input, by_sentence=True, preprocess=True, random=False, num_beams=4):
     direction = DIRECTION.get(dir)
     result = translator.translate(
-        text=text,
+        text=text_input,
         direction=direction,
-        do_sample=random, #parameter to enables decoding strategies
+        do_sample=random, #parameter to enable decoding strategies
         num_beams=int(num_beams),
         by_sentence=by_sentence,
         preprocess=preprocess,
@@ -17,22 +17,21 @@ def translate_wrapper(text, dir, by_sentence=True, preprocess=True, random=False
     return result
 
 
-article = """
+note = """
 This is a NLLB-200-600M model fine-tuned for translation between Truku and traditional Chinese languages.
 
 """
 
-
 interface = gr.Interface(
     translate_wrapper,
     [
-        gr.Textbox(label="Input Text", lines=2, placeholder='text to translate '),
         gr.Dropdown(DIRECTION_LIST, type="value", label='Translation direction', value=DIRECTION_LIST),
+        gr.Textbox(label="Input Text", lines=2, placeholder='text to translate '),
         gr.Checkbox(label="by sentence", value=True),
     ],
     "text",
     title='Truku-Chinese translation',
-    article=article,
+    article=note,
 )
 
 
