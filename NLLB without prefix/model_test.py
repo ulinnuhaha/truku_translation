c7440c069d491746049ae71c482da451e4517f22
Ulin Nuha <ulinpoenya@gmail.com>
2023-12-30 11:49:29 +0800

    Update model_test.py

--- NLLB without prefix/model_test.py
+++ NLLB without prefix/model_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+# import the libraries
 import os
 import torch
 import random
@@ -17,11 +17,11 @@ from transformers import (
     DataCollatorForSeq2Seq,
     Seq2SeqTrainer,
 )
-
+# use argparse to let the user provides values for variables at runtime
 def DataTestingArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name_or_path', 
-        type=str, required=True, help='Load a LLM as model checkpoint for translation')
+        type=str, required=True, help='Load a NLLB as model checkpoint for translation')
     parser.add_argument('--data_dir', 
         type=str, required=True, help='Directory of the dataset files')
     args = parser.parse_args()
@@ -32,14 +32,9 @@ def DataTestingArguments():
 class Config:
     lang: str = "tr_ch_no_prefix"
     seed: int = 42
-    max_source_length: int = 128
-    max_target_length: int = 128
+    max_source_length: int = 128 # the maximum length in number of tokens for tokenizing the input sentence
+    max_target_length: int = 128 # the maximum length in number of tokens for tokenizing the target sentence
     device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    def __post_init__(self):
-        random.seed(self.seed)
-        np.random.seed(self.seed)
-        torch.manual_seed(self.seed)
-        torch.cuda.manual_seed_all(self.seed)
 
 def main():
     data_test_args=DataTestingArguments()
@@ -59,7 +54,9 @@ def main():
                 data_files=data_file
             )
         return dataset_dict
+    # load the dataset of sentence pair of (Truku→Chinese)
     dataset_dict_tc=get_data('tc')
+    # load the dataset of sentence pair of (Chinese→Truku)
     dataset_dict_ct=get_data('ct')
     print(dataset_dict_tc)
     #Load the evaluation metrics
@@ -68,14 +65,14 @@ def main():
     sacrebleu_score = evaluate.load("sacrebleu")
     chrf_score = evaluate.load("chrf")
     
-    # Load the tokenizer and model from pre-trained translation
-    
+    # load the fine-tuned translation model
     model_name = data_test_args.model_name_or_path 
         
-    if os.path.isdir(model_name): #load the pre-trained translation model if available
+    if os.path.isdir(model_name): #load the fine-tuned translation model if available
         model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     else:
         print("pre-trained data not found")
+    # Load the tokenizer from the fine-tuned translation model
     tokenizer = NllbTokenizer.from_pretrained(model_name)
     
     def fix_tokenizer(tokenizer, new_lang='tru_Latn'):
@@ -102,31 +99,29 @@ def main():
     print("number of parameters:", model.num_parameters())
     def batch_tokenize_fn(examples):
         """
-        Generate the input_ids and labels field for dataset dict.
+        Generate the input_ids and labels field for dataset dict of training data.
         """
-        src = (list(examples.keys()))[0]
-        tgt = (list(examples.keys()))[1]
-        sources = examples[src]
-        targets = examples[tgt]
+        #set source (input) and target Languages
+        src = (list(examples.keys()))[0] # get the language of source (input)
+        tgt = (list(examples.keys()))[1] # get the language of target (output)
+        sources = examples[src] # get the input samples
+        targets = examples[tgt] # get the target samples
             
-        src = 'tru_Latn' if src == 'truku' else 'zho_Hant'
-        tgt = 'tru_Latn' if tgt == 'truku' else 'zho_Hant'
+        src = 'tru_Latn' if src == 'truku' else 'zho_Hant' # set the language tag of of source (input)
+        tgt = 'tru_Latn' if tgt == 'truku' else 'zho_Hant' # set the language tag of of target (output)
         
-        tokenizer.src_lang = src
-    
+        # tokenizing the input sentences
+        tokenizer.src_lang = src #The language to use as source language for translation
         model_inputs = tokenizer(sources, max_length=config.max_source_length, truncation=True)
     
-        # setup the tokenizer for targets,
-        # huggingface expects the target tokenized ids to be stored in the labels field
-        # note, newer version of tokenizer supports a text_target argument, where we can create
-        # source and target sentences in one go
-        #with tokenizer.as_target_tokenizer():
-        tokenizer.src_lang = tgt
+        # tokenizing the target sentences
+        # tokenized ids of the target are stored as the labels field
+        tokenizer.src_lang = tgt #The language to use as target language for translation
         labels = tokenizer(targets, max_length=config.max_target_length, truncation=True)
     
         model_inputs["labels"] = labels["input_ids"]
         return model_inputs
-        
+    # Tokenizing sentence pair of Truku and Chinese dataset   
     tc_dataset_dict_tokenized = dataset_dict_tc.map(
             batch_tokenize_fn,
             batched=True,
@@ -141,17 +136,17 @@ def main():
     # evalution metrics computation
     def compute_metrics(eval_pred):
         """
-        Compute rouge and bleu metrics for seq2seq model generated prediction.
+        Compute rouge, BERTscore, chrF, and bleu metrics for seq2seq model generated prediction.
         
-        tip: we can run trainer.predict on our eval/test dataset to see what a sample
+        tip: we can run trainer.predict on our eval dataset to see what a sample
         eval_pred object would look like when implementing custom compute metrics function
         """
         predictions, labels = eval_pred
-        # Decode generated summaries, which is in ids into text
+        # Decode prediction samples, which is in ids into text
         decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
         # Replace -100 in the labels as we can't decode them
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-        # Decode labels, a.k.a. reference summaries into text
+        # Decode tokenized labels a.k.a. reference translation into text
         decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
         result = rouge_score.compute(
             predictions=decoded_preds,
@@ -162,11 +157,9 @@ def main():
             predictions=decoded_preds,
             references=decoded_labels
         )
-        chrf=chrf_score.compute(predictions=decoded_preds, references=decoded_labels)
-        berts = bert_score.compute(predictions=decoded_preds, references=decoded_labels,  model_type="bert-base-chinese")
-        result["bert_score"]= np.mean(berts['f1'])
+        chrf=chrf_score.compute(predictions=decoded_preds, references=decoded_labels) ##The higher the value, the better the translations
         result["sacrebleu"] = score["score"] #The higher the value, the better the translations
-        result["chrf"] = chrf["score"]
+        result["chrf"] = chrf["score"] #The higher the value, the better the translations
         return {k: round(v, 4) for k, v in result.items()}
     
     #The training arguments for the training session, can be ignored since we do not perform training session
@@ -183,10 +176,10 @@ def main():
         )
     
     # perform the testing process
-    print('+++-----Testing stage for Truku to Chinese-----+++')
+    print('+++-----Testing stage for Truku to Chinese translation-----+++')
     pred_t2c=trainer.predict(tc_dataset_dict_tokenized["test"])
     print(pred_t2c[-1])
-    print('+++-----Testing stage for Chinese to Truku-----+++')
+    print('+++-----Testing stage for Chinese to Truku translation-----+++')
     pred_c2t=trainer.predict(ct_dataset_dict_tokenized["test"])
     print(pred_c2t[-1])
 if __name__ == "__main__":
